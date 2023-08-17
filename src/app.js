2e75bac4a6dc24c4e60fdd6a616cbfbf84dff296
sofia-chiodi <chiodi.sb@gmail.com>
2023-08-17 01:14:57 -0300

    17 - se crea estructura del sitio web

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-const app = express();
+const app = express(); // lo ejecutamos para obtener un obj con metodos
 
 app.use(express.static(path.join(__dirname, "../public"))); // archivos estaticos que se pueden usar en cualquier lado. Se le da acceso a toda la carpeta de una
 
@@ -11,7 +11,9 @@ app.listen(PORT, () => {
     console.log(`Se prendio en el puerto ${PORT}`);
 });
 
-app.get("/", (req, res) => {
+app.get("/", (req, res) => {     //ruta parametro 1, callback que se ejecutara al obtener la ruta (parametro 2)
     res.sendFile(path.join(__dirname, "views/home.html"));
-});
+}); 
+
+
 
