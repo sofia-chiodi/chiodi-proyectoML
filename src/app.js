8a70432b8d7c57f0dffbd37dc1044a5ff49532e9
sofia-chiodi <chiodi.sb@gmail.com>
2023-08-21 04:07:30 -0300

    Se arreglan estilos y se agregan archivos html

--- src/app.js
+++ src/app.js
@@ -1,19 +1,25 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express(); // lo ejecutamos para obtener un obj con metodos
 
 app.use(express.static(path.join(__dirname, "../public"))); // archivos estaticos que se pueden usar en cualquier lado. Se le da acceso a toda la carpeta de una
 
-const PORT = 3001;
+const PORT = 3002;
 
 app.listen(PORT, () => {
-    console.log(`Se prendio en el puerto ${PORT}`);
+  console.log(`Se prendio en el puerto ${PORT}`);
 });
 
-app.get("/", (req, res) => {     //ruta parametro 1, callback que se ejecutara al obtener la ruta (parametro 2)
-    res.sendFile(path.join(__dirname, "views/home.html"));
-}); 
-
-
-
+app.get("/", (req, res) => {
+  //ruta parametro 1, callback que se ejecutara al obtener la ruta (parametro 2)
+  res.sendFile(path.join(__dirname, "views/home.html"));
+});
+app.get("/login.html", (req, res) => {
+  //ruta parametro 1, callback que se ejecutara al obtener la ruta (parametro 2)
+  res.sendFile(path.join(__dirname, "views/login.html"));
+});
+app.get("/register.html", (req, res) => {
+  //ruta parametro 1, callback que se ejecutara al obtener la ruta (parametro 2)
+  res.sendFile(path.join(__dirname, "views/register.html"));
+});
