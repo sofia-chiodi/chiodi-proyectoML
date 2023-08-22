ce40d272216e89537ad0607129891916101b1a07
sofia-chiodi <chiodi.sb@gmail.com>
2023-08-21 21:16:41 -0300

    se actualiza el PORT

--- src/app.js
+++ src/app.js
@@ -5,10 +5,15 @@ const app = express(); // lo ejecutamos para obtener un obj con metodos
 
 app.use(express.static(path.join(__dirname, "../public"))); // archivos estaticos que se pueden usar en cualquier lado. Se le da acceso a toda la carpeta de una
 
-const PORT = 3002;
+// const PORT = 3000;
 
+// app.listen(PORT, () => {
+//   console.log(`Se prendio en el puerto ${PORT}`);
+// });
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Se prendio en el puerto ${PORT}`);
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
 
 app.get("/", (req, res) => {
