4b7805f2fdf38ec202d1f7618a00f70dc46ceebd
sofia-chiodi <chiodi.sb@gmail.com>
2023-09-19 09:56:40 -0300

    Actualizo entry-point y creo vista de errores 404

--- src/app.js
+++ src/app.js
@@ -1,20 +1,31 @@
+// Require's
 const express = require('express');
 const path = require('path');
 
+// Ejecucion de express
+const app = express();
+
+// Middlewares
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Template engines
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+// Sistema de routeo
 const mainRoutes = require('./routes/mainRoutes.js');
 const usersRoutes = require('./routes/usersRoutes.js');
 
-const app = express();
+app.use('/', mainRoutes);
+app.use('/users', usersRoutes);
 
-app.use(express.static(path.join(__dirname, '../public')));
+// Manejo de errores
+app.use((req, res, next) => {
+  res.status(404).render('not-found');
+});
 
+// Puerto
 const PORT = process.env.PORT || 3013;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
-
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
-
-app.use('/', mainRoutes);
-app.use('/users', usersRoutes);
