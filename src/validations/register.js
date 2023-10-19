060d94eff25b2d5a968a708c850e1f164bbb7f94
sofia-chiodi <chiodi.sb@gmail.com>
2023-10-19 16:32:42 -0300

    Actaulizo validaciones para register

--- src/validations/register.js
+++ src/validations/register.js
@@ -1,4 +1,5 @@
 const { body } = require('express-validator');
+const path = require('path');
 
 module.exports = [
   body('fullName').notEmpty().withMessage('Ingresá tu nombre'),
@@ -9,12 +10,7 @@ module.exports = [
     .isEmail()
     .withMessage('Ingresá un correo electrónico válido'),
   body('address').notEmpty().withMessage('Ingresá tu dirección'),
-  body('birthDate')
-    .notEmpty()
-    .withMessage('Ingresá tu fecha de nacimiento')
-    .bail()
-    .isDate()
-    .withMessage('Ingresá una fecha válida'),
+  body('birthDate').notEmpty().withMessage('Ingresá tu fecha de nacimiento'),
   body('username').notEmpty().withMessage('Ingresá un nombre de usuario'),
   body('password')
     .notEmpty()
@@ -28,4 +24,22 @@ module.exports = [
     .bail()
     .isLength({ min: 8 })
     .withMessage('La contraseña debe tener al menos 8 caracteres'),
+  body('profilePicture').custom((value, { req }) => {
+    let file = req.file;
+    let acceptedExtensions = ['.jpg', '.png', '.gif'];
+
+    if (!file) {
+      throw new Error('Subí una foto de perfil');
+    } else {
+      let fileExtension = path.extname(file.originalname);
+      if (!acceptedExtensions.includes(fileExtension)) {
+        throw new Error(
+          `Las extensiones de archivo permitidas son ${acceptedExtensions.join(
+            ', '
+          )}`
+        );
+      }
+    }
+    return true;
+  }),
 ];
